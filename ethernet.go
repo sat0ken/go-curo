b54c0a567cc0e5c30d2c91ab897b52b594c39c0e
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-01-28 13:37:25 +0900

    update

--- ethernet.go
+++ ethernet.go
@@ -8,6 +8,7 @@ import (
 const ETHER_TYPE_IP uint16 = 0x0800
 const ETHER_TYPE_ARP uint16 = 0x0806
 const ETHER_TYPE_IPV6 uint16 = 0x86dd
+const ETHERNET_ADDRES_LEN = 6
 
 var ETHERNET_ADDRESS_BROADCAST = [6]uint8{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
 
@@ -23,6 +24,14 @@ func setMacAddr(macAddrByte []byte) [6]uint8 {
 		macAddrUint8[i] = v
 	}
 	return macAddrUint8
+}
+
+func setIPAddr(ipAddrByte []byte) [4]uint8 {
+	var ipAddrUint8 [4]uint8
+	for i, v := range ipAddrByte {
+		ipAddrUint8[i] = v
+	}
+	return ipAddrUint8
 }
 
 func byteToUint16(b []byte) uint16 {
@@ -46,6 +55,7 @@ func (netdev netDevice) ethernetInput(packet []byte) {
 	case ETHER_TYPE_ARP:
 		fmt.Println("packet is ARP")
 		// Todo: ARPパケットを処理する関数を呼ぶ
+		netdev.arpInput(packet[14:])
 	case ETHER_TYPE_IP:
 		fmt.Println("packet is IP")
 		// Todo: IPパケットを処理する関数を呼ぶ
