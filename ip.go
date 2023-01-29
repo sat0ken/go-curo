fbefd6c80dc25c650cbe3457cbcf922957686d96
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-01-29 14:03:22 +0900

    add ip.go

--- ip.go
+++ ip.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"net"
+	"strings"
+)
+
+const IP_ADDRESS_LEN = 4
+
+type ipDevice struct {
+	address   uint32 // デバイスのIPアドレス
+	netmask   uint32 // サブネットマスク
+	broadcast uint32 // ブロードキャストアドレス
+}
+
+func getIPdevice(addrs []net.Addr) (ipdev ipDevice) {
+	for _, addr := range addrs {
+		// ipv6ではなくipv4アドレスをリターン
+		ipaddrstr := addr.String()
+		if !strings.Contains(ipaddrstr, ":") && strings.Contains(ipaddrstr, ".") {
+			ip, ipnet, _ := net.ParseCIDR(ipaddrstr)
+			ipdev.address = byteToUint32(ip.To4())
+			ipdev.netmask = byteToUint32(ipnet.Mask)
+			// ブロードキャストアドレスの計算はIPアドレスとサブネットマスクのbit反転の2進数「OR（論理和）」演算
+			ipdev.broadcast = ipdev.address | (^ipdev.netmask)
+		}
+	}
+	return ipdev
+}
