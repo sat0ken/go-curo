c8d7b00ec2ec3196484d639085f13aa304c9dcb0
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-02-04 07:42:56 +0900

    add utils.go

--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package main
 
-func byteArraySum(packet []byte) (sum uint) {
+func sumByteArr(packet []byte) (sum uint) {
 	for i, _ := range packet {
 		if i%2 == 0 {
 			sum += uint(byteToUint16(packet[i:]))
@@ -9,7 +9,11 @@ func byteArraySum(packet []byte) (sum uint) {
 	return sum
 }
 
-func calcChecksum(packet []byte) {
+func calcChecksum(packet []byte) []byte {
 	// まず16ビット毎に足す
-	packetsum := byteArraySum(packet)
+	sum := sumByteArr(packet)
+	// あふれた桁を足す
+	sum = (sum & 0xffff) + sum>>16
+	// 論理否定を取った値をbyteにして返す
+	return uint16ToByte(uint16(sum ^ 0xffff))
 }
