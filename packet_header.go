262898ab5dda15a3a405fb3f7f8dd107f755ae93
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-02-27 07:35:39 +0900

    add packet_header.go

--- packet_header.go
+++ packet_header.go
@@ -3,9 +3,18 @@ package main
 type udpHeader struct {
 	srcPort  uint16
 	destPort uint16
+	length   uint16
+	checksum uint16
 }
 
 type tcpHeader struct {
-	srcPort  uint16
-	destPort uint16
+	srcPort    uint16
+	destPort   uint16
+	seq        uint32
+	ackseq     uint32
+	offset     uint8
+	tcpflag    uint8
+	window     uint16
+	checksum   uint16
+	urgPointer uint16
 }
