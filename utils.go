5d6b0f6e9c9bab20fa7a9515af3da53ee31b3bdc
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-05-06 07:41:38 +0900

    functionを移動

--- utils.go
+++ utils.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/binary"
+	"fmt"
+	"strings"
+)
+
 func sumByteArr(packet []byte) (sum uint) {
 	for i, _ := range packet {
 		if i%2 == 0 {
@@ -16,4 +22,32 @@ func calcChecksum(packet []byte) []byte {
 	sum = (sum & 0xffff) + sum>>16
 	// 論理否定を取った値をbyteにして返す
 	return uint16ToByte(uint16(sum ^ 0xffff))
+}
+
+func printMacAddr(macddr [6]uint8) string {
+	var str string
+	for _, v := range macddr {
+		str += fmt.Sprintf("%x:", v)
+	}
+	return strings.TrimRight(str, ":")
+}
+
+func byteToUint16(b []byte) uint16 {
+	return binary.BigEndian.Uint16(b)
+}
+
+func byteToUint32(b []byte) uint32 {
+	return binary.BigEndian.Uint32(b)
+}
+
+func uint16ToByte(i uint16) []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, i)
+	return b
+}
+
+func uint32ToByte(i uint32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, i)
+	return b
 }
