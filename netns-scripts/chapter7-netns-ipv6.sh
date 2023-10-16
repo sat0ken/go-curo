aeb9dafe7aaa60d5b6c85b0bafda1ad9e9f7c404
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-10-16 09:56:08 +0900

    スクリプト更新

--- netns-scripts/chapter7-netns-ipv6.sh
+++ netns-scripts/chapter7-netns-ipv6.sh
@@ -13,30 +13,22 @@ ip -all netns delete
 
 # 4つのnetnsを作成
 ip netns add host1
-ip netns add host2
 ip netns add router1
 ip netns add router2
-ip netns add router3
-ip netns add host3
+ip netns add host2
 
 # リンクの作成
-ip link add name host1-router1 type veth peer name router1-host1      # host1とrouter1のリンク
-ip link add name router1-router2 type veth peer name router2-router1  # router1とrouter2のリンク
-ip link add name router2-host2 type veth peer name host2-router2      # router2とhost2のリンク
-ip link add name router1-router3 type veth peer name router3-router1  # router1とrouter3のリンク
-ip link add name router3-host3 type veth peer name host3-router3      # router3とhost3のリンク
+ip link add name host1-router1 type veth peer name router1-host1 # host1とrouter1のリンク
+ip link add name router1-router2 type veth peer name router2-router1 # router1とrouter2のリンク
+ip link add name router2-host2 type veth peer name host2-router2 # router2とhost2のリンク
 
 # リンクの割り当て
 ip link set host1-router1 netns host1
 ip link set router1-host1 netns router1
 ip link set router1-router2 netns router1
-ip link set router1-router3 netns router1
 ip link set router2-router1 netns router2
-ip link set router3-router1 netns router3
 ip link set router2-host2 netns router2
 ip link set host2-router2 netns host2
-ip link set router3-host3 netns router3
-ip link set host3-router3 netns host3
 
 # host1のリンクの設定
 ip netns exec host1 ip addr add 2001:db8:0:1::2/64 dev host1-router1
@@ -51,13 +43,7 @@ ip netns exec router1 ethtool -K router1-host1 rx off tx off
 ip netns exec router1 ip addr add 2001:db8::1/64 dev router1-router2
 ip netns exec router1 ip link set router1-router2 up
 ip netns exec router1 ethtool -K router1-router2 rx off tx off
-ip netns exec router1 ip addr add 2001:db8:0:3::1/64 dev router1-router3
-ip netns exec router1 ip link set router1-router3 up
-ip netns exec router1 ethtool -K router1-router3 rx off tx off
-ip netns exec router1 ip route add 2001:db8:0:2::1/64 via 2001:db8::2
-#if [ "$FLAG" == "" ]; then
-ip netns exec router1 ip route add 2001:db8:0:4::1/64 via 2001:db8:0:3::2
-#fi
+#ip netns exec router1 ip route add 2001:db8::1/64 via 2001:db8::2
 
 # router2のリンクの設定
 ip netns exec router2 ip addr add 2001:db8::2/64 dev router2-router1
@@ -74,27 +60,10 @@ ip netns exec host2 ip link set host2-router2 up
 ip netns exec host2 ethtool -K host2-router2 rx off tx off
 ip netns exec host2 ip route add default via 2001:db8:0:2::1
 
-# router3のリンクの設定
-ip netns exec router3 ip addr add 2001:db8:0:3::2/64 dev router3-router1
-ip netns exec router3 ip link set router3-router1 up
-ip netns exec router3 ethtool -K router3-router1 rx off tx off
-ip netns exec router3 ip route add 2001:db8:0:1::1/64 via 2001:db8:0:3::1
-ip netns exec router3 ip addr add 2001:db8:0:4::1/64 dev router3-host3
-ip netns exec router3 ip link set router3-host3 up
-ip netns exec router3 ethtool -K router3-host3 rx off tx off
-
-# host3のリンクの設定
-ip netns exec host3 ip addr add 2001:db8:0:4::2/64 dev host3-router3
-ip netns exec host3 ip link set host3-router3 up
-ip netns exec host3 ethtool -K host3-router3 rx off tx off
-ip netns exec host3 ip route add default via 2001:db8:0:4::1
-
-# routerを起動する場合はipv6フォワードを無効にする
+# icmpv6を無視
 if [ "$FLAG" == "-router" ]; then
-  ip netns exec router1 sysctl -w net.ipv6.icmp.echo_ignore_all=1
   ip netns exec router1 sysctl -w net.ipv6.conf.all.forwarding=0
 else
   ip netns exec router1 sysctl -w net.ipv6.conf.all.forwarding=1
 fi
-ip netns exec router2 sysctl -w net.ipv6.conf.all.forwarding=1
-ip netns exec router3 sysctl -w net.ipv6.conf.all.forwarding=1
+ip netns exec router1 ip link set router1-router2 mtu 1300
---
+++ netns-scripts/chapter8-netns-ipv6.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# rootユーザーが必要
+if [ $UID -ne 0 ]; then
+  echo "Root privileges are required"
+  exit 1;
+fi
+
+# 全てのnetnsを削除
+ip -all netns delete
+
+# 3つのnetnsを作成
+ip netns add host1
+ip netns add router1
+ip netns add host2
+
+# リンクの作成
+ip link add name host1-router1 type veth peer name router1-host1 # host1とrouter1のリンク
+ip link add name host2-router1 type veth peer name router1-host2 # router1とhost2のリンク
+
+# リンクの割り当て
+ip link set host1-router1 netns host1
+ip link set router1-host1 netns router1
+ip link set host2-router1 netns host2
+ip link set router1-host2 netns router1
+
+# host1のリンクの設定
+ip netns exec host1 ip addr add 2001:db8:0:1::2/64 dev host1-router1
+ip netns exec host1 ip link set host1-router1 up
+ip netns exec host1 ethtool -K host1-router1 rx off tx off
+ip netns exec host1 ip route add default via 2001:db8:0:1::1
+
+# router1のリンクの設定
+ip netns exec router1 ip addr add 2001:db8:0:1::1/64 dev router1-host1
+ip netns exec router1 ip link set router1-host1 up
+ip netns exec router1 ethtool -K router1-host1 rx off tx off
+ip netns exec router1 ip addr add 192.168.0.1/24 dev router1-host2
+ip netns exec router1 ip link set router1-host2 up
+ip netns exec router1 ethtool -K router1-router2 rx off tx off
+
+# host2のリンクの設定
+ip netns exec host2 ip addr add 192.168.0.2/24 dev host2-router1
+ip netns exec host2 ip link set host2-router1 up
+ip netns exec host2 ethtool -K host2-router1 rx off tx off
+ip netns exec host2 ip route add default via 192.168.0.1
+
+ip netns exec router1 sysctl -w net.ipv4.conf.all.forwarding=0
+ip netns exec router1 sysctl net.ipv4.icmp_echo_ignore_all=1
+ip netns exec router1 sysctl -w net.ipv6.conf.all.forwarding=0
+ip netns exec router1 sysctl -w net.ipv6.icmp.echo_ignore_all=1
